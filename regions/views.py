493b29d39e56da90f1cb830b6345a39d3b839dfc
kolbukm <m.kolbuk@student.mini.pw.edu.pl>
2017-10-30 00:19:32 +0100

    added regions main view

--- regions/views.py
+++ regions/views.py
@@ -1,3 +1,9 @@
+from django.template import  RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+def regions(request):
+    return render(request,"regions/regions.html")
+    
