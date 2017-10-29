e6ff8b86c875e38e781dda86671cc006a88567ad
Artur Kozieł <artur.koziel@rentalsunited.com>
2017-10-29 19:23:28 +0100

    Add models for regions and updates

--- regions/models.py
+++ regions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class GeoLocalization(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    class Meta:
+        proxy = True
+
+
+
+class Region(models.Model):
+    north_west = models.OneToOneField(GeoLocalization, on_delete=models.CASCADE, parent_link=True)
+    north_east = models.OneToOneField(GeoLocalization, on_delete=models.CASCADE, parent_link=True)
+    south_west = models.OneToOneField(GeoLocalization, on_delete=models.CASCADE, parent_link=True)
+    north_east = models.OneToOneField(GeoLocalization, on_delete=models.CASCADE, parent_link=True)
