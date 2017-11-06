f31ad59fd486646775907fd6df17eddd72483ca0
apuacz <alicja.puacz@gmail.com>
2017-11-06 10:27:44 +0100

    login and regions tests, regions options

--- regions/views.py
+++ regions/views.py
@@ -1,9 +1,21 @@
 from django.template import  RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def regions(request):
-    return render(request,"regions/regions.html")
-    
+	return render(request,"regions/regions.html")
+		
+
+def manualActualization(request):
+	return HttpResponse("Ręczna aktualizacja stanu dróg")
+	
+
+def actualUpdates(request):
+	return HttpResponse("Wyświetl aktualnie przeprowadzane aktualizacje")
+	
+	
+def updatesHistory(request):
+	return HttpResponse("Historia aktualizacji")
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class UserTest(TestCase):
+
+    def loginTest(self):
+		self.credentials = {
+            'username': 'admin',
+            'password': 'Fd9Km_'}
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+		
+	def loginFailTest(self):
+		self.credentials = {
+            'username': 'admin00',
+            'password': 'admin123'}
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
