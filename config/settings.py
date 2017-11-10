f0b5defa0878f076829a63d11e22491471eca753
Bartosz Nowicki <nowis1337@gmail.com>
2017-11-11 00:41:00 +0100

    added some whitenoise configuration

--- config/settings.py
+++ config/settings.py
@@ -154,6 +154,8 @@ STATIC_ROOT = 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'var', 'media')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
