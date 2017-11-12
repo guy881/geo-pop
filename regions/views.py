7bec8b0b0012ca1c0a5f0666a929d49e60f56d31
Paweł <pawel.mucha@yahoo.pl>
2017-11-12 21:08:14 +0100

    Fix indentation Remove unused imports Add LoginRequiredMixin to RegionsView RegionsView as main url

--- regions/views.py
+++ regions/views.py
@@ -1,30 +1,28 @@
-from django.template import  RequestContext
-from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import GeoLocalization, Region
 from drivers.models import Driver
 
-class RegionsView(TemplateView):
+
+class RegionsView(LoginRequiredMixin, TemplateView):
     template_name = "regions/regions.html"
-    
+
+
 class AddDriverToRegionView(TemplateView):
     template_name = "regions/add_driver_to_region.html"
-     
+
     def get_queryset(self, request):
         drivers = Driver.objects.all()
         return drivers
-		
-        
+
+
 def manualActualization(request):
-	return HttpResponse("Ręczna aktualizacja stanu dróg")
-	
+    return HttpResponse("Ręczna aktualizacja stanu dróg")
+
 
 def actualUpdates(request):
-	return HttpResponse("Wyświetl aktualnie przeprowadzane aktualizacje")
-	
-	
+    return HttpResponse("Wyświetl aktualnie przeprowadzane aktualizacje")
+
+
 def updatesHistory(request):
-	return HttpResponse("Historia aktualizacji")
+    return HttpResponse("Historia aktualizacji")
