fe7a2f45baed3e30240bf7c70a509750d7dd5337
Artur Kozieł <artur.koziel@rentalsunited.com>
2017-11-12 22:14:02 +0100

    added repositories for cars, regions, drivers

---
+++ cars/repository.py
@@ -0,0 +1,20 @@
+from cars.models import Car
+
+
+class CarsRepository:
+
+    @staticmethod
+    def add_car(car):
+        car.save()
+
+    @staticmethod
+    def update_car(car):
+        car.save()
+
+    @staticmethod
+    def delete_car(car):
+        car.delete()
+
+    @staticmethod
+    def get_all_cars():
+        return Car.objects.all
