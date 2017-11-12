7d6de6172ac61b3c7ec0831404cbe295bdc1e6f7
a_Kwasny <andrzej.kwasiborski@gmail.com>
2017-11-12 21:08:48 +0100

    minor visual changes

--- static/js/script.js
+++ static/js/script.js
@@ -28,16 +28,17 @@ function nameSurname(string) {
     }
 }
 
-function showWrong(field, message, glyph) {
+function showWrong(field, message, glyph, glyph_positive) {
     field.innerHTML = message;
     glyph.style.visibility = "visible";
+    glyph_positive.style.visibility = "hidden";
     return false;
 }
 
 function showOK(field, glyph, glyph_positive) {
     field.innerHTML = "";
     glyph.style.visibility = "hidden";
-    glyph_positive.style.visibility = "visible"
+    glyph_positive.style.visibility = "visible";
     return true;
 }
 
@@ -45,14 +46,15 @@ function check_full_name(form_name) {
     var full_name = document.forms[form_name]["full_name"].value;
     var field = document.getElementById("full_name_error");
     var glyph = document.getElementById("full_name_glyph");
+    var glyph_positive = document.getElementById("full_name_glyph_positive");
     if (isEmpty(full_name)) {
-        showWrong(field, empty_message, glyph)
+        showWrong(field, empty_message, glyph, glyph_positive)
     } else if (full_name.length < 6) {
-        showWrong(field, too_short_message, glyph)
+        showWrong(field, too_short_message, glyph, glyph_positive)
     } else if (nameSurname(full_name)) {
-        showWrong(field, white_space_message, glyph)
+        showWrong(field, white_space_message, glyph, glyph_positive)
     } else {
-        showOK(field, glyph)
+        showOK(field, glyph, glyph_positive)
     }
 }
 
@@ -60,12 +62,13 @@ function check_gender(form_name) {
     var gender = document.forms[form_name]["gender"].value;
     var field = document.getElementById("gender_error");
     var glyph = document.getElementById("gender_glyph");
+    var glyph_positive = document.getElementById("gender_glyph_positive");
     if (isEmpty(gender)) {
-        showWrong(field, empty_message, glyph)
+        showWrong(field, empty_message, glyph, glyph_positive)
     } else if (gender == "default") {
-        showWrong(field, default_message, glyph)
+        showWrong(field, default_message, glyph, glyph_positive)
     } else {
-        showOK(field, glyph)
+        showOK(field, glyph, glyph_positive)
     }
 }
 
@@ -86,12 +89,13 @@ function check_pesel(form_name) {
     var pesel = document.forms[form_name]["pesel"].value;
     var field = document.getElementById("pesel_error");
     var glyph = document.getElementById("pesel_glyph");
+    var glyph_positive = document.getElementById("pesel_glyph_positive");
     if (isEmpty(pesel)) {
-        showWrong(field, empty_message, glyph)
+        showWrong(field, empty_message, glyph, glyph_positive)
     } else if (pesel.length !== 11) {
-        showWrong(field, too_short_message, glyph)
+        showWrong(field, too_short_message, glyph, glyph_positive)
     } else {
-        showOK(field, glyph)
+        showOK(field, glyph, glyph_positive)
     }
 }
 
@@ -99,12 +103,13 @@ function check_phone_number(form_name) {
     var phone_number = document.forms[form_name]["phone_number"].value;
     var field = document.getElementById("phone_number_error");
     var glyph = document.getElementById("phone_number_glyph");
+    var glyph_positive = document.getElementById("phone_number_glyph_positive");
     if (isEmpty(phone_number)) {
-        showWrong(field, empty_message, glyph)
+        showWrong(field, empty_message, glyph, glyph_positive)
     } else if (phone_number.length !== 9) {
-        showWrong(field, too_short_message, glyph)
+        showWrong(field, too_short_message, glyph, glyph_positive)
     } else {
-        showOK(field, glyph)
+        showOK(field, glyph, glyph_positive)
     }
 }
 
@@ -112,12 +117,13 @@ function check_permissions_level(form_name) {
     var permissions_level = document.forms[form_name]["permissions_level"].value;
     var field = document.getElementById("permissions_level_error");
     var glyph = document.getElementById("permissions_level_glyph");
+    var glyph_positive = document.getElementById("permissions_level_glyph_positive");
     if (isEmpty(permissions_level)) {
-        showWrong(field, empty_message, glyph)
+        showWrong(field, empty_message, glyph, glyph_positive)
     } else if (permissions_level == "default") {
-        showWrong(field, default_message, glyph)
+        showWrong(field, default_message, glyph, glyph_positive)
     } else {
-        showOK(field, glyph)
+        showOK(field, glyph, glyph_positive)
     }
 }
 
