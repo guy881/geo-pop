3a8f3008ca0ba50887f8f9715565434be3d1f383
okraskaj <okraskaj@ee.pw.edu.pl>
2017-12-03 15:32:01 +0100

    work in progress on api. Maybe this will help someone untill I finish

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,8 @@ CUSTOM_APPS = [
 
 PLUGINS = [
     'versatileimagefield',
-    'rest_framework'
+    'rest_framework',
+    'django_extensions',
 ]
 
 INSTALLED_APPS = BASE_APPS + CUSTOM_APPS + PLUGINS
@@ -134,8 +135,8 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAdminUser',
+    ],
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework.authentication.BasicAuthentication',
     #     'rest_framework.authentication.SessionAuthentication',
---
+++ users/serializers.py
@@ -0,0 +1,9 @@
+# Serializers define the API representation.
+from rest_framework import serializers
+from users.models import CustomUser
+
+
+class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('url', 'email', 'is_staff')
